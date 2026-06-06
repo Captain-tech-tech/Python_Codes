8b7f52806c72258fa6cc7a8dd354802c3134bc7c
Captain-tech-tech <mmatif1112@gmail.com>
2026-06-06 21:44:42 +0500

    code for finding memory size of the variables

---
+++ practiceQuestions/memorysize.py
@@ -0,0 +1,14 @@
+# important concept
+
+# memory size is return in bytes
+
+# import sys
+# a = 10
+# b = 3.14
+# c = "Hello"
+# print(sys.getsizeof(a))
+# print(sys.getsizeof(b))
+# print(sys.getsizeof(c))
+# # syntax 
+# import sys
+# sys.getsizeof(variable)
