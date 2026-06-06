72ffca5ff765b077a89038e5d317b6e786adf5cf
Captain-tech-tech <mmatif1112@gmail.com>
2026-06-06 21:51:11 +0500

    code using concept of tuple and while loop to search for a number

---
+++ practiceQuestions/search_for_number.py
@@ -0,0 +1,19 @@
+
+# using tuple and while loop to search for a number
+
+# tup=(45,576,23,56,34,4523)
+
+# n=int(input("Enter the number for which you are searching : "))
+
+# count=0
+# not_find=1
+
+# while count<len(tup):
+#     if(n==tup[count]):
+#         print("The number ",n,"is found at index : ",count)
+#         not_find=0
+#         break
+#     count+=1    
+# if(not_find):
+#     print("Number is not present in the tuple!")
+
