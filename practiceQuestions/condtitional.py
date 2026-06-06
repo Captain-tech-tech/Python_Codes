54b559103598606f77e9ada82d3cee1aea15ef2a
Captain-tech-tech <mmatif1112@gmail.com>
2026-06-06 21:49:25 +0500

    conditional statement realted question and finding multiplication of a number

---
+++ practiceQuestions/condtitional.py
@@ -0,0 +1,26 @@
+
+# practice question on conditional statements
+"""
+a=int(input("Enter your age : "))
+if a>18 and a<=80:  #  if(a>18 and a<=80)  also can be written like this
+    print("You are eligible for voting and driving")
+elif a>80:
+    print("Sorry Sir, you are too old, kindly let someone else to drive")
+elif a<18 and a>10:
+    print("Brother you have a little time to drive and vote according to your interest")
+else : 
+    print("Okay, son you can't vote and drive, but you are allowed to fly your father's money")
+"""
+
+
+
+
+# multiplication of a number
+
+# n=int(input("Enter a number : "))
+# count=1
+# while count<=10:
+#     print(n,"*",count," = ",count*n)
+#     count+=1
+
+
